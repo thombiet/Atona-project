879ec4ebbb7a412a5092ec430ccec93b74b48f1f
thalie7 <nathalie.maissiat@gmail.com>
2016-10-02 00:28:43 +0200

    Add files via upload
    Le retour du IndexController

---
+++ GESTBANK_Atona/src/main/java/com/wha/springmvc/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.wha.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+	  @RequestMapping(method = RequestMethod.GET)
+	    public String getIndexPage() {
+	        //return "UserManagement";
+		  return "Formulaire_Inscription";
+	    }
+
+}
