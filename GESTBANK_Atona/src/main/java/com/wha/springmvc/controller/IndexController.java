a2dbfbef76b968abb0eee27aad5f41e31a92fb5d
Nathalie <nathalie.maissiat@gmail.com>
2016-10-05 09:39:29 +0200

    dgdgfhjj

---
+++ GESTBANK_Atona/src/main/java/com/wha/springmvc/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.wha.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+	  @RequestMapping(method = RequestMethod.GET)
+	    public String getIndexPage() {
+	      //return "UserManagement";
+		  return "testService";
+		  //return "AccueilGuest2";
+	  }
+
+}
